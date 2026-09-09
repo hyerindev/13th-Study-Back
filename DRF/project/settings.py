6082e2df059493550879feb738b66be81f07f484
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-09 22:01:17 +0900

    CORS_ORIGIN_WHITELIST 수정

--- DRF/project/settings.py
+++ DRF/project/settings.py
@@ -168,6 +168,8 @@ SIMPLE_JWT = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
     'http://127.0.0.1:8080',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
     'https://HyerinStudyBack13thLikelion.pythonanywhere.com',
 )
 
