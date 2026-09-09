d32dde53ee501e5869c3c3e52b754d864cab8251
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-09 21:49:53 +0900

    CORS 추가

--- Dfirstproject/firstproject/settings.py
+++ Dfirstproject/firstproject/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'community.apps.CommunityConfig',
     'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
